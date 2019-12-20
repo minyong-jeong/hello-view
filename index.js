8db57a7390ac4fa050428fb7bc44e2e11d8d54c3
Minyong Jeong <jmy3155@gmail.com>
2019-12-21 01:38:08 +0900

    Add filter table view

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const terminalCard = document.getElementById("terminal");
 const digitalClockCard = document.getElementById("digital-clock");
+const filterTableCard = document.getElementById("filter-table");
 
 terminalCard.addEventListener('click', () => {
     location.href = "./terminal/terminal.html"
@@ -7,4 +8,8 @@ terminalCard.addEventListener('click', () => {
 
 digitalClockCard.addEventListener('click', () => {
     location.href = "./digital_clock/digital_clock.html"
+});
+
+filterTableCard.addEventListener('click', () => {
+    location.href = "./filter_table/filter_table.html"
 });
