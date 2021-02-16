d328e4aef98e4cf77d649cb98b4a1cadff03f815
Minyong Jeong <jmy3155@gmail.com>
2021-02-17 00:21:16 +0900

    Update project structure

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-document.getElementById("terminal").onclick = () => location.href = "./terminal/index.html";
-document.getElementById("digital-clock").onclick = () => location.href = "./digital_clock/index.html";
-document.getElementById("filter-table").onclick = () => location.href = "./filter_table/index.html";
-document.getElementById("analog-clock").onclick = () => location.href = "./analog_clock/index.html";
-document.getElementById("custom-buttons").onclick = () => location.href = "./custom_buttons/index.html";
-document.getElementById("color-picker").onclick = () => location.href = "./color_picker/index.html";
+document.getElementById("terminal").onclick = () => location.href = "./examples/Terminal/index.html";
+document.getElementById("digital-clock").onclick = () => location.href = "./examples/DigitalClock/index.html";
+document.getElementById("filter-table").onclick = () => location.href = "./examples/FilterTable/index.html";
+document.getElementById("analog-clock").onclick = () => location.href = "./examples/AnalogClock/index.html";
+document.getElementById("custom-buttons").onclick = () => location.href = "./examples/CustomButtons/index.html";
+document.getElementById("color-picker").onclick = () => location.href = "./examples/ColorPicker/index.html";
