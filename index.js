aafc14c1ef1e3f1bc4489d7dcf67f49c17032076
Minyong Jeong <jmy3155@gmail.com>
2021-02-17 00:08:48 +0900

    Add color picker to index page

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ document.getElementById("digital-clock").onclick = () => location.href = "./digi
 document.getElementById("filter-table").onclick = () => location.href = "./filter_table/index.html";
 document.getElementById("analog-clock").onclick = () => location.href = "./analog_clock/index.html";
 document.getElementById("custom-buttons").onclick = () => location.href = "./custom_buttons/index.html";
+document.getElementById("color-picker").onclick = () => location.href = "./color_picker/index.html";
