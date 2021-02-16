5c3451f3bd1c681b8dab88fd5475d2366799ea3e
Minyong Jeong <jmy3155@gmail.com>
2021-02-17 00:04:45 +0900

    Update color picker

--- color_picker/index.js
+++ color_picker/index.js
@@ -2,35 +2,46 @@ const box = document.getElementById("box");
 const redInput = document.getElementById("red");
 const greenInput = document.getElementById("green");
 const blueInput = document.getElementById("blue");
-const hexText = document.getElementById("hex-text");
-const redLabel = document.getElementById("red-label");
-const greenLabel = document.getElementById("green-label");
-const blueLabel = document.getElementById("blue-label");
-
 let red = 0;
 let green = 0;
 let blue = 0;
-
-redInput.addEventListener("input", (e) => {
-    red = redInput.value;
-    box.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
-    hexText.innerHTML = "Hex (" + rgbToHex() + ")";
-    redLabel.innerHTML = "R (" + red + ")";
-});
-
-greenInput.addEventListener("input", (e) => {
-    green = greenInput.value;
-    box.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
-    greenLabel.innerHTML = "G (" + green + ")";
-});
-
-blueInput.addEventListener("input", (e) => {
-    blue = blueInput.value;
-    box.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
-    blueLabel.innerHTML = "B (" + blue + ")";
-});
 
 const rgbToHex = () => {
     const rgb = (red << 16) | (green << 8) | (blue << 0);
     return '#' + (0x1000000 + rgb).toString(16).slice(1);
 }
+
+const colorChange = () => {
+    document.getElementById("hex-text").innerHTML = "Hex (" + rgbToHex() + ")";
+    document.getElementById("red-label").innerHTML = "R (" + red + ")";
+    document.getElementById("green-label").innerHTML = "G (" + green + ")";
+    document.getElementById("blue-label").innerHTML = "B (" + blue + ")";
+    box.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+}
+
+const randomColor = () => {
+    redInput.value = red = Math.floor(Math.random() * 256);
+    greenInput.value = green = Math.floor(Math.random() * 256);
+    blueInput.value = blue = Math.floor(Math.random() * 256);
+}
+
+redInput.addEventListener("input", (e) => {
+    red = redInput.value;
+    colorChange();
+});
+
+greenInput.addEventListener("input", (e) => {
+    green = greenInput.value;
+    colorChange();
+});
+
+blueInput.addEventListener("input", (e) => {
+    blue = blueInput.value;
+    colorChange();
+});
+
+window.onload = () => {
+    randomColor();
+    colorChange();
+}
+
